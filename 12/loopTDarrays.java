cdeda329a5b1e76b1240d141cded67ee22e08cdd
webctory <md.mofazzalhossain18@gmail.com>
2023-05-20 15:23:49 +0600

    3-pm

--- 12/loopTDarrays.java
+++ 12/loopTDarrays.java
@@ -1,5 +1,52 @@
 public class loopTDarrays {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
+    //     int[][] grades = {
+    //      {23,34,34},
+    //      {43,54,23,54},
+    //      {45,44,23,75,545,5445}
+    //     };
+
+    //     for (int j = 0; j<grades[0].length; j++) {
+    //         System.out.print(" "+grades[0][j]);
+    //     }
+
+    //     System.out.print("\n");
+
+    //     for (int j = 0; j < grades[1].length; j++) {
+    //         System.out.print(" "+grades[1][j]);
+    //     }
+
+    //     System.out.print("\n");
+
+    //     for (int j = 0; j < grades[1].length; j++) {
+    //         System.out.print(" "+grades[2][j]);
+    //     }
+
+    //     System.out.print("\n");
+
         
+    // }
+
+    public static void main(String[] args) {
+
+        int[][] grades = {
+
+         {23,34,34},
+         {43,54,23,54},
+         {45,44,23,75,545,5445}
+        };
+        
+        for (int i =0; i < grades.length; i++) {
+
+            switch(i) {
+                case 0: System.out.println("\tHarry: "); break;
+                case 1: System.out.println("\tHarry: "); break;
+                case 2: System.out.println("\tbake: "); break;
+            }
+            for (int j = 0; j < grades.length; j++) {
+                System.out.println(grades[i][j]+ " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
---
+++ 12/wb6_14.java
@@ -0,0 +1,35 @@
+/**
+ * wb6_14
+ */
+public class wb6_14 {
+
+    public static void main(String[] args) {
+
+        int[][] array = new int[100][10];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = randomNumber();
+            }
+        }
+        print2DArray(array);
+    }
+
+    public static int randomNumber() {
+
+        double randomNumber = Math.random()*100;
+        return (int)randomNumber;
+        
+    }
+
+    public static void print2DArray(int[][] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.println(array[i][j]+" ");
+            }
+            System.out.println("\n");
+        }
+        
+    }
+}
