50e95a6363dad9b64f90f66a451be93aab841f48
webctory <md.mofazzalhossain18@gmail.com>
2023-05-22 18:44:09 +0600

    Javapedia is done

--- 12/javapedia.java
+++ 12/javapedia.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class javapedia {
@@ -12,6 +13,7 @@ public class javapedia {
 
          scan.nextLine();
         for (int i =0; i < database.length; i++) {
+            System.out.println("\n\tFigure: "+(i+1));
             System.out.print("\t . Name: ");
             database[i][0] = scan.nextLine();
             System.out.print("\t . Date of birth: ");
@@ -22,7 +24,33 @@ public class javapedia {
 
         }
 
+        System.out.println("These are the values you sored: ");
+        print2Darray(database);
+        System.out.println("\nWho do you want information on?");
+
+        //scan.nextLine();
+        String name = scan.nextLine();
+        for (int i =0; i < database.length; i++) {
+            if (database[i][0].equals(name)) {
+                System.out.println("\tName: "+ database[i][0]);
+                System.out.println("\tDate of birth: "+ database[i][1]);
+                System.out.println("\tOccupation: "+ database[i][2]);
+                
+            }
+        }
+
          
+    }
+
+    public static void print2Darray(String[][] array)  { 
+        for (int i =0; i < array.length; i++ ) {
+            System.out.println("\t");
+            for (int j = 0; j < array[i].length; j++ ) {
+                System.out.println((array[i][j])+" ");
+                //
+            }
+            System.out.println("\n");
+        }        
     }
     
 }
